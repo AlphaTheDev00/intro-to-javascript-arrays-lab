c0c86b5af3e2ae690c4f02d53818ea5124caac79
AlphaTheDev00 <chikar.yassine@gmail.com>
2024-12-04 14:23:47 +0000

    exercise intro to js

---
+++ app.js
@@ -0,0 +1,180 @@
+/*
+Exercise 1: Define an empty array
+
+1) Create an empty array and assign it to a variable called `foods`.
+
+Exercise 1 has been completed for you:
+*/
+
+//const foods = [];  
+//foods.unshift('pizza', 'cheeseburger');
+//console.log('Exercise 1 result:', foods);
+
+
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+Complete Exercise 3 in the space below:
+*/
+
+// let foods = ['pizza', 'cheeseburger'];
+
+// foods.unshift('taco');
+
+// console.log('Exercise 3 result:', foods);
+
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+// let foods = ['taco', 'pizza', 'cheeseburger'];
+
+// let favFood = foods[1];
+
+// console.log('Exercise 4 result:', favFood);
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+// let foods = ['taco', 'pizza', 'cheeseburger'];
+
+// foods.splice(2, 0, 'tofu');
+
+// console.log('Exercise 5 result:', foods);
+
+
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+// let foods = ['taco', 'pizza', 'cheeseburger'];
+
+// foods.splice(1, 1, 'sushi', 'cupcake');
+
+// console.log('Exercise 6 result:', foods);
+
+
+/*
+Exercise 8: Finding an index
+
+1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
+   `foods` array. 
+
+2) Assign it to a variable named `soyIdx`.
+
+Complete Exercise 8 in the space below:
+*/
+
+// let foods = ['taco', 'tofu' ,'pizza', 'cheeseburger'];
+
+// let soyIdx = foods.indexOf('tofu');
+
+// console.log('Exercise 8 result:', soyIdx);
+
+
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+// let foods = ['taco', 'tofu' ,'pizza', 'cheeseburger'];
+
+// let allFoods = foods.join(' -> ');
+
+
+// console.log('Exercise 9 result:', allFoods);
+
+
+/*
+Exercise 10: Check for an element
+
+1) Using the .includes() method, check if the `foods` array contains the string
+   'soup'.
+
+2) Assign the result to a variable called `hasSoup``.
+
+Complete Exercise 10 in the space below:
+*/
+// let foods = ['taco', 'tofu' ,'pizza', 'cheeseburger'];
+
+// hasSoup = foods.includes('soup');
+
+// console.log('Exercise 10 result:', hasSoup);
+
+
+
+/*
+Exercise 11: Odd numbers from an array
+
+1) Choose a method to iterate through the `nums` array.
+
+2) Push each odd number to a new array named `odds`.
+
+Hint: Initialize the `odds` variable to an empty array before the iteration.
+
+Complete Exercise 11 in the space below:
+*/
+
+// const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+
+// let odds = [];
+
+// for (let i = 0; i < nums.length; i++) {
+ 
+//     if (nums[i] % 2 !== 0){
+//         odds.push(nums[i]);
+//     }
+// }
+
+
+
+/*
+Exercise 12: FizzBuzz with arrays
+
+1) Choose a method to iterate through the `nums` array. 
+
+2. As you loop, sort the numbers into new arrays based on the following rules:
+
+   - Push any number evenly divisible by 3 to an array called `fizz`.
+   - Push any number evenly divisible by 5 to an array called `buzz`.
+   - Push any number that is evenly divisible by 3 and 5 to an array called
+     `fizzbuzz`.
+
+   Note: A single number may meet more than one of the above rules. If it does,
+         it should be placed in multiple arrays. For example, the number `15`
+         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
+
+Complete Exercise 12 in the space below:
+*/
+
+
+
+console.log('Exercise 12 Results:');
+console.log('  fizz:', fizz);
+console.log('  buzz:', buzz);
+console.log('  fizzbuzz:', fizzbuzz);
